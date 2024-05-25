4cc4adaa7678e9fe07598b916d33a9f08d0ad7d2
furkanvarekar <92787509+furkanvarekar19@users.noreply.github.com>
2024-05-25 16:14:15 +0530

    update

---
+++ script.js
@@ -0,0 +1,81 @@
+// Question 3:
+// Create a JavaScript function that takes two arrays as input and returns a new array containing only 
+// the elements that are present in both arrays (intersection).
+// Input:
+// [1, 2, 3, 4]
+// [2, 4, 6, 8]
+// Output:
+// [2, 4]
+
+// var array = [1,2,3,4];
+// var array1 = [2,4,6,8];
+
+// function intersection(array,array1){
+//     for(var i =0;i<array.length;i++){
+//         for(var j=0;j<array1.length;j++){
+//             if(array[i]===array1[j])
+//                var array3= push(array[i])
+//         }
+
+//     }
+//    return array3;
+// }
+
+// var final = [1,2,3,4],[2,4,6,8];
+// console.log(final);
+
+
+
+// Question 5:
+// Create a JavaScript function that takes an array of numbers and returns a new array with all the
+//  duplicate elements removed.
+// Input:
+// [1, 2, 2, 3, 4, 4, 5]
+// Output:
+// [1, 2, 3, 4, 5]
+
+// var array=[1, 2, 2, 3, 4, 4, 5];
+
+// function duplicateRemove(){
+//  return array.filter()
+
+// }
+
+
+
+
+
+// Question 1:
+// Write a JavaScript for loop that iterates from 1 to 100 and prints "Fizz" for multiples of 3, 
+// "Buzz" for multiples of 5, and "FizzBuzz" for multiples of both 3 and 5.
+
+//  for(var i =1;i<=100;i++){
+//     // console.log(i);
+//     if(i%3===0 && i%5){
+//         console.log("fizzbuzz");
+//     }else if(i%3){
+//         console.log("buzz");
+
+//     }else if(i%5){
+//         console.log("fizz");
+// e
+//     }else{
+//         console.log(i);
+//     }
+//  }
+
+
+// Question 2:
+
+// Write a JavaScript function that takes a string as input and returns the character that appears the
+//  most frequently. If multiple characters have the same frequency, return the first one.
+
+
+
+
+
+
+
+
+
+
